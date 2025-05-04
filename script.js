25bf26286705d3c2620f6f191844058efdf33cac
Elainpojatkissa <elainpojatkissa@gmail.com>
2025-05-04 18:06:12 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Esimerkki kooditehtävien interaktiivisuudesta
+document.addEventListener('DOMContentLoaded', () => {
+    // Tässä voisi olla kooditehtävien listaus ja logiikka
+    const tasksSection = document.getElementById('tasks');
+    tasksSection.innerHTML += '<p>Tehtäviä lisätään pian...</p>';
+});
